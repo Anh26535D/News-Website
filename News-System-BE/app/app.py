96db7407e2dc25c492c45130966ed11b9f29dbc5
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2024-01-09 23:08:23 +0700

    add gen data

--- News-System-BE/app/app.py
+++ News-System-BE/app/app.py
@@ -1,11 +1,5 @@
-import os
 from flask import Flask
-from Service.crawl import Fetching
-from Service.hdfs import hdfs
 from routers.news import news_blueprint
-import sys
-
-PATH_DATA = './app/data'
 
 
 def create_app():
@@ -15,21 +9,4 @@ def create_app():
 
     app.register_blueprint(news_blueprint,url_prefix = "/news")
 
-    # hdfs_service = hdfs()
-    # crawl_service = Fetching(api_key=app.config.get("NEWS_API_KEY"),url_fetch=app.config.get("FECTH_DATA_URL"))
-
-    # #create base data
-    # # Get a list of file names in the specified path
-    # file_names = [f for f in os.listdir(PATH_DATA) if os.path.isfile(os.path.join(PATH_DATA, f))]
-    # if len(file_names)==0:
-    #     file_paths=crawl_service.crawlData()
-    #     if file_paths:
-    #         for file_path in file_paths:
-    #             hdfs.put_news(file_path,url=f'{app.config["HDFS_API_URL"]}/fsputnew')
-    # else :
-    #     files_in_hdfs = hdfs.ls('news',url=f'{app.config.get("HDFS_API_URL")}/fsls/')['file_names']
-    #     file_names = [item for item in file_names if f'app_data_{item}' not in files_in_hdfs]
-    #     for file_path in file_names:
-    #         hdfs.put_news(f'{PATH_DATA}/{file_path}',url=f'{app.config["HDFS_API_URL"]}/fsputnew')
-    
     return app
---
+++ News-System-BE/generate_data.py
@@ -0,0 +1,82 @@
+import os
+import dotenv
+import requests
+
+from pymongo import MongoClient
+from faker import Faker
+
+
+dotenv.load_dotenv()
+
+
+mongodb_uri = os.environ.get('MONGO_URI')
+newsapi_apikey = os.environ.get('NEWSAPI_APIKEY')
+db_mongo_name = os.environ.get('DB_NAME')
+collection_mongo_name = os.environ.get('COLLECTION_NAME')
+
+
+class NewsDataGenerator:
+
+    def __init__(self, mongodb_uri=None, newsapi_apikey=None, num_news=100) -> None:
+        if mongodb_uri is None:
+            raise Exception("Mongodb URI is not provided")
+        self.mongodb_uri = mongodb_uri
+        self.newsapi_apikey = newsapi_apikey
+        self.num_news = num_news
+        self.fake = Faker()
+
+    def run(self):
+        if self.newsapi_apikey is None:
+            print("Start fetching data by random...")
+            self.fetch_data_by_random()
+            print("Done")
+        else:
+            print("Start fetching data from API...")
+            self.fetch_data_from_api()
+            print("Done")
+
+    def fetch_data_from_api(self):
+        url = f"https://newsapi.org/v2/everything?q=apple&sortBy=popularity&apiKey={newsapi_apikey}"
+
+        client = MongoClient(mongodb_uri)
+        db = client[str(db_mongo_name)]
+        collection = db[str(collection_mongo_name)]
+
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            response_json = response.json()
+            articles = response_json['articles']
+            collection.insert_many(articles)
+        else:
+            raise Exception("Error when fetching data from API")
+        client.close()
+
+    def random_news(self):
+        news = {
+            "source": {
+                "id": "", 
+                "name": self.fake.company()
+            }, 
+            "author": self.fake.name(), 
+            "title": self.fake.sentence(), 
+            "description": self.fake.paragraph(), 
+            "url": self.fake.url(), 
+            "urlToImage": self.fake.image_url(), 
+            "publishedAt": self.fake.date_time_this_decade().isoformat() + "Z",
+            "content": self.fake.text()
+        }
+
+        return news
+
+    def fetch_data_by_random(self, num_news=100):
+        client = MongoClient(mongodb_uri)
+        db = client[str(db_mongo_name)]
+        collection = db[str(collection_mongo_name)]
+
+        news = [self.random_news() for _ in range(num_news)]
+        collection.insert_many(news)
+        client.close()
+
+data_gen = NewsDataGenerator(mongodb_uri, None)
+data_gen.run()
